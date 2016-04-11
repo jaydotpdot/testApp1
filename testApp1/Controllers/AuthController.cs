99d97b76f007305b5077c4815d0f4dfe2810faca
jaydotpdot <Jason.Phillips@lan.local>
2016-04-11 16:36:13 -0600

    Added basic login form

--- testApp1/Controllers/AuthController.cs
+++ testApp1/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using testApp1.ViewModels;
 
 namespace testApp1.Controllers
 {
@@ -10,7 +11,29 @@ namespace testApp1.Controllers
     {
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            {
+                //Test = "This is my test value set in my controller!"
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if (form.Username != "Jay")
+            {
+                ModelState.AddModelError("Username", "Username or password isn't correct.");
+                return View(form);
+            }
+
+            return Content("The form is valid!");
+
+            //form.Test = "This is a value set in my POST action";
+
+            //return View(form);
         }
     }
 }
---
+++ testApp1/ViewModels/Auth.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testApp1.ViewModels
+{
+    public class AuthLogin
+    {
+        //public string Test { get; set; }
+
+        // data annotations
+        [Required]
+        public string Username { get; set; }
+
+        // data annotations
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
