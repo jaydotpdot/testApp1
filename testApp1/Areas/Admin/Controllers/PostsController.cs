f92d5950f0556752932f85be4ad6e5209350963a
jaydotpdot <jasongordonphillips@gmail.com>
2016-04-15 17:51:10 -0600

    Put together basic admin layout

--- testApp1/Areas/Admin/Controllers/PostsController.cs
+++ testApp1/Areas/Admin/Controllers/PostsController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using testApp1.Infrastructure;
 
 namespace testApp1.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         // GET: Admin/Posts
         public ActionResult Index()
         {
-            return Content("ADMIN POSTS!");
+            //return Content("ADMIN POSTS!");
+            return View();
         }
     }
 }
--- testApp1/Areas/Admin/Controllers/UsersController.cs
+++ testApp1/Areas/Admin/Controllers/UsersController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using testApp1.Infrastructure;
 
 namespace testApp1.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return Content("USERS!");
+            //return Content("USERS!");
+            return View();
         }
     }
 }
